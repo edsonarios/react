9c1d264f453ed37c62e86908ce47e728e2158d84
edsonarios <edsonrios9@gmail.com>
2023-05-10 11:00:26 -0400

    Changed style to: Add new Item

--- src/sections/todos/footer/footer.styles.ts
+++ src/sections/todos/footer/footer.styles.ts
@@ -3,21 +3,23 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 export const Wrapper = styled.button`
   margin-top: 8px;
-  padding: 4px;
+  padding: 8px;
   display: flex;
   align-items: center;
   background-color: transparent;
+  margin-left:14px;
   border: none;
 `;
 
 export const AddIcon = styled(AddCircleOutlineIcon)`
   color: #3D82EB;
-  widht: 12px;
-  height: 12px;
+  widht: 24px;
+  height: 24px;
 `
 
 export const AddText = styled.span`
   color: #3D82EB;
-  font-size: 10px;
+  font-size: 24px;
   padding: 0 12px;
+  margin-left:10px;
 `
