5e02c83c91efb24ab2dd3dfdee83634ee93bfdd4
edsonarios <edsonrios9@gmail.com>
2023-05-10 17:57:24 -0400

    Fix date for actual day

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -2,7 +2,7 @@ const Calendar = () => {
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     const date = new Date();
     const month = months[date.getMonth()];
-    const day = date.getDay()
+    const day = date.getDate()
 
     return (
         <div className='calendar'>
